69627d680775c62a2464d6b8bf98450004407180
Hassan ABIDAR <abidar069716@gmail.com>
2025-12-04 13:51:05 +0100

    Activity Model/Service/Rep/Controllers setup are done and 2 apis are working and tested on postman

---
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -0,0 +1,30 @@
+package com.fitness.activityservice.controller;
+
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.service.ActivityService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/activities")
+@AllArgsConstructor
+public class ActivityController {
+
+    private ActivityService activityService;
+
+    @PostMapping
+    private ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
+        return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+    @GetMapping
+    private ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId){
+        return ResponseEntity.ok(activityService.getUserActivities(userId));
+    }
+}
---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,51 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+    public ActivityResponse trackActivity(ActivityRequest request){
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+
+    }
+    private ActivityResponse mapToResponse(Activity activity){
+        ActivityResponse activityResponse = new ActivityResponse();
+        activityResponse.setId(activity.getId());
+        activityResponse.setUserId(activity.getUserId());
+        activityResponse.setType(activity.getType());
+        activityResponse.setDuration(activity.getDuration());
+        activityResponse.setCaloriesBurned(activity.getCaloriesBurned());
+        activityResponse.setStartTime(activity.getStartTime());
+        activityResponse.setAdditionalMetrics(activity.getAdditionalMetrics());
+        activityResponse.setCreatedAt(activity.getCreatedAt());
+        activityResponse.setUpdatedAt(activity.getUpdatedAt());
+        return activityResponse;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+            List<Activity> activities = activityRepository.findByUserId(userId);
+            return activities.stream().map(this::mapToResponse).collect(Collectors.toList());
+    }
+}
